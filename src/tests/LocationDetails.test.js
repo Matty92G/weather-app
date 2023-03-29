cf9beb714f7a591d657ffce507647e0979297c2a
Matthew Giles <matty92g@hotmail.co.uk>
2023-03-29 22:09:06 +0100

    Added Testing for LocationDetails and App

--- src/tests/LocationDetails.test.js
+++ src/tests/LocationDetails.test.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { render } from "@testing-library/react";
+import LocationDetails from "../components/LocationDetails";
 
 describe("LocationDetails", () => {
-  it("renders", () => {});
+  it("renders the correct city and location props", () => {
+    const { getByText } = render(
+      <LocationDetails city="Manchester" country="UK" />
+    );
+    expect(getByText("Manchester, UK")).toBeInstanceOf(HTMLHeadingElement);
+  });
 });
