d94b8c9765918d94c843673aafd8e774784bfa9e
Matthew Giles <matty92g@hotmail.co.uk>
2023-04-01 15:32:52 +0100

    More Testing for ForecastSummary

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -5,8 +5,10 @@ function ForecastSummary(props) {
   return (
     <div className="forecast-summary">
       <div className="forecast-summary_date">{date}</div>
-      <div className="forecast-summary_icon">{icon}</div>
-      <div className="forecast-summary_temperature">{temperature}</div>
+      <div className="forecast-summary_icon" data-testid="forecast-icon">
+        {icon}
+      </div>
+      <div className="forecast-summary_temperature">{temperature}°C</div>
       <div className="forecast-summary_description">{description}</div>
     </div>
   );
--- src/tests/ForecastSummary.test.js
+++ src/tests/ForecastSummary.test.js
@@ -12,7 +12,7 @@ describe("ForecastSummary", () => {
       max: 99,
     },
   };
-  it("renders", () => {
+  it("renders and matches snapshot", () => {
     const { asFragment } = render(
       <ForecastSummary
         date={validProps.date}
@@ -22,5 +22,21 @@ describe("ForecastSummary", () => {
       />
     );
     expect(asFragment).toMatchSnapshot();
+  });
+  it("renders correct values for props", () => {
+    const { getByText, getByTestId } = render(
+      <ForecastSummary
+        date={validProps.date}
+        icon={validProps.icon}
+        description={validProps.description}
+        temperature={validProps.temperature.max}
+      />
+    );
+    expect(getByText("11111111")).toHaveClass("forecast-summary_date");
+    expect(getByTestId("forecast-icon")).toHaveClass("forecast-summary_icon");
+    expect(getByText("stubDescription")).toHaveClass(
+      "forecast-summary_description"
+    );
+    expect(getByText("99°C")).toHaveClass("forecast-summary_temperature");
   });
 });
