a819422d2f1d57268523c5822f6b09c3c4b03054
Matthew Giles <matty92g@hotmail.co.uk>
2023-04-10 21:17:12 +0100

    Fixed Axios via reset VS Code and changed endpoint to /forecast to retrive data required

--- src/tests/components/ForecastSummaries.test.js
+++ src/tests/components/ForecastSummaries.test.js
@@ -3,33 +3,41 @@ import { render } from "@testing-library/react";
 import ForecastSummaries from "../../components/ForecastSummaries";
 
 describe("ForecastSummaries", () => {
-  const validProps = [
-    {
-      date: 11111111,
-      icon: "stubIcon1",
-      description: "stubDescription1",
-      temperature: {
-        min: 10,
-        max: 91,
+  const validProps = {
+    forecasts: [
+      {
+        date: 11111111,
+        icon: "stubIcon1",
+        description: "stubDescription1",
+        temperature: {
+          min: 10,
+          max: 91,
+        },
       },
-    },
-    {
-      date: 22222222,
-      icon: "stubIcon2",
-      description: "stubDescription2",
-      temperature: {
-        min: 20,
-        max: 92,
+      {
+        date: 22222222,
+        icon: "stubIcon2",
+        description: "stubDescription2",
+        temperature: {
+          min: 20,
+          max: 92,
+        },
       },
-    },
-  ];
+    ],
+    onForecastSelect: () => {},
+  };
   it("renders and matches snapshot", () => {
-    const { asFragment } = render(<ForecastSummaries forecasts={validProps} />);
+    const { asFragment } = render(
+      <ForecastSummaries
+        forecasts={validProps.forecasts}
+        onForecastSelect={validProps.onForecastSelect}
+      />
+    );
     expect(asFragment()).toMatchSnapshot();
   });
   it("renders the correct number of ForecastSummary instances", () => {
     const { getAllByTestId } = render(
-      <ForecastSummaries forecasts={validProps} />
+      <ForecastSummaries forecasts={validProps.forecasts} />
     );
     expect(getAllByTestId("forecast-summary")).toHaveLength(2);
   });
